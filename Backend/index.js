1fa733c2ceefc1fd6ef62d200a621b2c657f3b7b
thitiwat-ak <thitiwat.ak@ku.th>
2026-02-27 13:05:28 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -16,10 +16,134 @@ server.listen(port,host,()=>{
 
 const express = require('express');
 const bodyParser = require('body-parser')
+const mysql = require('mysql2/promise')
 const app = express();
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 const port = 8000;
+
+
+
+const initMySQL = async() =>{
+    conn = await mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'root',
+        database: 'webdbb',
+        port:8700
+    });
+    console.log('Connected to MySQL databese');
+}
+
+// path = get/users สำหรับ ดึงข้อมูล users ทั้งหมเด
+
+app.get('/users', async (req,res)=>{
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
+})
+
+//path: = POST /users สำหรับเพิ่ม  user ใหม่ 
+
+app.post ('/users', async (req,res)=>{
+    try {
+        let user = req.body;
+        const results = await conn.query('INSERT INTO users SET ?', [user]); // ลองใส่ [ ] ครอบ user
+        res.json({
+        message: 'User added successfully',
+        data: results[0]
+
+    });
+    } catch (error) {
+        console.error('Error inserting user:',error);
+        res.status(500).json({ message: 'Error adding user'});
+    }   
+})
+//path =GET/users/:id ดึงข้อมูล user ตาม id;
+app.get('/users/:id', async (req,res)=>{
+    try{
+        let id = req.params.id;
+        const results =await conn.query('SELECT * FROM users WHERE id = ?',id);
+        if (result[0].length ===0){
+            throw { statusCode:404, message: 'User not found' };
+        }
+        res.json(results[0][0]);
+
+    }catch (error){
+        console.error('Error fetching user:',error);
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message: error.message || 'Error fetching user'
+        });
+    }
+})
+
+//path: = PUT /users/:id อัพเดทข้อมูล user ตาม รก
+app.put('/user/:id', async (req,res)=>{
+    try{
+        let id = req.params.id;
+        let updateUser = req.body;
+        const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updateUser, id]);
+        res.json({
+            message: 'User updated successfully',
+            data: results[0]
+        });
+    }catch (error){
+        console.error('Error deleting user:',error);
+        res.status(500).json({message: 'Error deleting user'});
+    }
+    
+})
+
+
+
+app.get('/testdb-new', async (req,res) =>{
+    try{
+        const conn = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database:'webdbb',
+            port: 8700
+        });
+        const results =await conn.query('SELECT *FROM users');
+        res.json(results[0]);
+
+    }catch (error){
+        console.error('Error connecting to the database:',error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+/*app.get('/testdb',(req,res) => {
+    mysql.createConnection({
+        host: 'localhost',
+        user:'root',
+        password: 'root',
+        database: 'webdbb',
+        port: 8700
+
+    }).then((conn) => {
+        conn
+        .query('SELECT * FROM users')
+        .then((results) => {
+            res.json(results[0]);
+        }).catch((err)=>{
+            res.json({ error: err.message});
+        
+        });
+    })
+})
+app.get('/testdb-new', async (req,res)=>{
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'root',
+        database:'webdbb',
+        port: 8700
+    });
+    const result = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
+}) */
 
 
 let users = [];
@@ -83,7 +207,7 @@ app.patch('/user/:id', (req,res)=>{
     //ส่ง users ที่อัพเดทแล้วกลับไป
     
 })
-app.delete('/users+/:id',(req,res)=>{
+app.delete('/users/:id',(req,res)=>{
     let id =req.params.id;
 
     // หา index จาก id ที่ต้องการลบ
@@ -103,7 +227,8 @@ app.delete('/users+/:id',(req,res)=>{
 });*/
 
 
-app.listen(port, () => {
+app.listen(port,  async() => {
+    await initMySQL();
     console.log(`Server is running on http://localhost:${port}`);
 
-});
+});     
